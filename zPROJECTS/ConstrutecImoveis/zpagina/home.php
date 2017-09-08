79be5d8fe9a82b23383630e71f4c84838677b79b
Dejota Freitas <djxpgs@hotmail.com>
2017-09-08 13:00:46 -0300

    s

--- zPROJECTS/ConstrutecImoveis/zpagina/home.php
+++ zPROJECTS/ConstrutecImoveis/zpagina/home.php
@@ -18,38 +18,49 @@ $dados = CRUD::select('endereco e, imovel i', '*', $where, $where_dados);
   span { margin-bottom: 5px; }
   .rotulo { font-weight: bold;}
   .row { margin-bottom: 10px; }
+  .imovel { font-size: 16px; font-weight: bold; }
+  .icone { height: 100px; }
 </style>
 
 <?php foreach ($dados as $key => $value): ?>
-  
-  <div class="row z-depth-1 selecionados grey lighten-2">
+
+  <div class="row z-depth-1 selecionados grey lighten-2 imovel">
 
     <?php if ($value['categoria']=='casa'): ?>
-      <span class="col s3 green-text rotulo">CASA</span>
+      <div class="col s2">
+        <img src="img/casa.png" class="icone col s12">
+        <span class="col s12 center-align green-text rotulo">CASA</span>
+      </div>
     <?php elseif ($value['categoria']=='ap'): ?>
-      <span class="col s3 purple-text rotulo">APARTAMENTO</span>
+      <div class="col s2">
+        <img src="img/ap.png" class="icone col 12">
+        <span class="col s12 center-align purple-text rotulo">APARTAMENTO</span>
+      </div>
     <?php elseif ($value['categoria']=='comercial'): ?>
-      <span class="col s3 pink-text rotulo">PONTO COMERCIAL</span>
+      <div class="col s2">
+        <img src="img/comercial.png" class="icone col s12">
+        <span class="col s12 center-align pink-text rotulo">COMERCIAL</span>
+      </div>
     <?php endif; ?>
 
     <span class="col s3"><span class="blue-text text-darken-4">Valor R$</span>: <?php echo $value['valor']; ?></span>
 
     <?php if (!empty($value['referencia'])): ?>
       <span class="col s6"><span class="blue-text text-darken-4">Referência</span>: <?php echo $value['referencia']; ?></span>
-    <?php endif; ?> 
+    <?php endif; ?>
 
     <span class="col s3"><span class="blue-text text-darken-4">Rua</span>: <?php echo $value['rua']; ?></span>
-    
+
     <span class="col s3"><span class="blue-text text-darken-4">Número</span>: <?php echo $value['numero'];
      ?></span>
 
     <span class="col s3"><span class="blue-text text-darken-4">Bairro</span>: <?php echo $value['bairro'];
-      ?></span>  
+      ?></span>
 
     <?php if ($value['quarto']!='0'): ?>
       <span class="col s3"><span class="blue-text text-darken-4">Quarto</span>: <?php echo $value['quarto']; ?></span>
     <?php endif; ?>
-    
+
     <?php if ($value['suite']!='0'): ?>
       <span class="col s3"><span class="blue-text text-darken-4">Suite</span>: <?php echo $value['suite']; ?></span>
     <?php endif; ?>
@@ -63,15 +74,15 @@ $dados = CRUD::select('endereco e, imovel i', '*', $where, $where_dados);
     <?php endif; ?>
 
     <?php if ($value['quintal']!='0'): ?>
-      <span class="col s3">Quintal</span>
+        <span class="col s3"><span class="blue-text text-darken-4">Quintal</span>: Sim</span>
     <?php endif; ?>
 
     <?php if ($value['varanda']!='0'): ?>
-      <span class="col s3">Varanda</span>
+      <span class="col s3"><span class="blue-text text-darken-4">Varanda</span>: Sim</span>
     <?php endif; ?>
 
     <?php if (!empty($value['descricao'])): ?>
-      <span class="col s6"><span class="blue-text text-darken-4">Descrição</span>: <?php echo $value['descricao']; ?></span>
+      <span class="col s9"><span class="blue-text text-darken-4">Descrição</span>: <?php echo $value['descricao']; ?></span>
     <?php endif; ?>
 
     <?php if (!empty($_SESSION['usuario'])): ?>
@@ -79,31 +90,31 @@ $dados = CRUD::select('endereco e, imovel i', '*', $where, $where_dados);
       <div class="row col s12">
       <br>
           <form class="" action="logado_alterar_valor/<?php echo $value['id']; ?>" method="post" enctype="multipart/form-data">
-          
+
           <div class="input-field col s2">
             <input id="cd_valor" name="valor" type="number" required step="0.01" oninput="if(this.value.length>10) this.value = this.value.slice(0, 10);" >
             <label for="cd_valor">Novo Valor R$</label>
           </div>
-          <button type="submit" name="submit" class="btn purple">Alterar Valor</button>      
-          
+          <button type="submit" name="submit" class="btn purple">Alterar Valor</button>
+
         <a href="logado_excluir/<?php echo $value['id']; ?>"><button type="button" name="button" class="btn red">excluir</button></a>
         <a href="logado_alterar_dados/<?php echo $value['id']; ?>"><button type="button" name="button" class="btn blue">Alterar Dados</button></a>
-        
+
         <?php if ($value['status']=='indisponivel'): ?>
           <a href="logado_alterar_disponivel/<?php echo $value['id']; ?>"><button type="button" name="button" class="btn orange">Estar Indisponivel</button></a>
         <?php else: ?>
           <a href="logado_alterar_indisponivel/<?php echo $value['id']; ?>"><button type="button" name="button" class="btn green">Estar Disponivel</button></a>
         <?php endif; ?>
 
-        
-        </form>              
+
+        </form>
       </div>
 
-      
+
     <?php endif; ?>
-    
-
-  </div>   
 
 
-<?php endforeach; ?>    
+  </div>
+
+
+<?php endforeach; ?>
