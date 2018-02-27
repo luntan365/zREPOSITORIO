e7fb60938abfe05537cc42deb52664a4d120ffae
DejotaFreitas <djxpgs@gmail.com>
2018-02-26 22:12:39 -0300

    s

--- C++/01_BASE/03_entrada_saida.cpp
+++ C++/01_BASE/03_entrada_saida.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
-using namespace std; 
+using namespace std;
 int main() {
 
   int num = 0;
   cin >> num;
   cout << "o numero "<< num << " foi insirdo por vc." << endl;
 
-  
-  cout << hex << 10 + 50 << endl; //saida em exadecimal
-  
 
-  
+  cout << hex << 10 + 50 << endl; //saida em exadecimal
+
+
+
   system("pause");
-  return 0;  
+  return 0;
 }
